794e6b7f00a689f61357d5201703db1f5b3fb963
Everton19 <evertontp5@gmail.com>
2025-08-02 17:02:44 -0300

    mostrando menssagens de erro ou sucesso ao atualizar usuario

--- src/app/components/user-infos/user-infos.component.ts
+++ src/app/components/user-infos/user-infos.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UpdateUserService } from '../../services/update-user.service';
 
 @Component({
   selector: 'app-user-infos',
@@ -8,10 +9,23 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './user-infos.component.scss'
 })
 export class UserInfosComponent {
+  private _updateUserService = inject(UpdateUserService);
+
   userInfosForm = new FormGroup({
     name: new FormControl(''),
     email: new FormControl(''),
     username: new FormControl(''),
     password: new FormControl(''),
-  })
+  });
+
+  updateUser(){
+    this._updateUserService.update(this.userInfosForm.value as any).subscribe({
+      next: () => {
+        this.userInfosForm.setErrors({ 'update-success': true });
+      },
+      error: () => {
+        this.userInfosForm.setErrors({ 'update-error': true });
+      }
+    })
+  }
 }
